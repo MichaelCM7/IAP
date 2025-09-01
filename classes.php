5b2a2dabf4a39510efb382d38f07f46fd82d0802
Michael Mbugua <michael.mbugua@strathmore.edu>
2025-09-01 09:25:05 +0300

    comment out copied text

--- classes.php
+++ classes.php
@@ -10,15 +10,15 @@
     }
   }
 
-  //call the class
+  // //call the class
 
-  //create an instance
-  $sample = new Sample();
+  // //create an instance
+  // $sample = new Sample();
 
-  //call the greet method
-  echo $sample->greet();
+  // //call the greet method
+  // echo $sample->greet();
 
-  //call the week_day method
-  echo $sample->week_day()
+  // //call the week_day method
+  // echo $sample->week_day()
 
 ?>
