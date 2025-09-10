ead7d90aa4cbbd23086c9898566bb9faeef0fc77
Michael Mbugua <michael.mbugua@strathmore.edu>
2025-09-10 11:10:08 +0300

    modify mail.php

--- Forms/forms.php
+++ Forms/forms.php
@@ -3,15 +3,18 @@ class forms {
     public function signup() {
  ?>
  <form action='submit.php' method='post'>
-    <label for='firstname'>First name:</label>
+    <label for='firstname'>First name</label><br>
     <input type='text' id='firstname' name='firstname' required><br><br>
-    <label for='lastname'>Last name:</label>
+    <label for='lastname'>Last name</label><br>
     <input type='text' id='lastname' name='lastname' required><br><br>
-    <label for='email'>Email:</label>
+    <label for='email'>Email</label><br>
     <input type='email' id='email' name='email' required><br><br>
-    <label for='password'>Password:</label>
+    <label for='password'>Password</label><br>
     <input type='password' id='password' name='password' required><br><br>
-    <?php echo $this->submit_button('Sign Up'); ?> <a href='login.php'>Already have an account? Log in</a>
+    <label for="confirmPassword">Confirm Password</label><br>
+    <input type="text" id="confrimPassword" name="confirmPassword" required><br><br>
+    <?php echo $this->submit_button('Sign Up'); ?> 
+    <p>Already have an account? <a href='login.php'>Log in</a></p>
 </form>
 <?php
     }
--- conf.php
+++ conf.php
@@ -3,7 +3,7 @@
   $conf['site_timezone'] = 'AFRICA/NAIROBI';
 
   // Site information
-  $conf['site_name'] = 'Project Necker'; //Necker Cube 
+  $conf['site_name'] = 'Project Impossible'; //Impossible Cube Logo
   $conf['site_url'] = 'http://localhost/IAP/';
   $conf['site_email'] = 'info@projectnecker.com';
 
@@ -14,7 +14,7 @@
   $conf['DB_TYPE'] = 'mysqli';
   $conf['DB_HOST'] = 'localhost';
   $conf['DB_USER'] = 'root';
-  $conf['DB_PASS'] = '';
+  $conf['DB_PASS'] = '77092295';
   $conf['DB_NAME'] = 'IAP';
 
   // print '<br>'
